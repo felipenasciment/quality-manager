c6a099dcc1551175cefd17c732bb437e27c4f0de
emanuelguimaraes23@gmail.com <emanuelguimaraes23@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-06-22 04:39:46 +0000

    Commit da versão mais nova do quality-manager_ANDROID

---
+++ quality-manager_ANDROID/src/br/edu/ifpb/LoginActivity.java
@@ -0,0 +1,73 @@
+package br.edu.ifpb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity implements OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		Button confirmar = (Button) findViewById(R.id.confirmar);
+		confirmar.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) 
+	{
+		EditText usuarioTextView = (EditText) findViewById(R.id.nome);
+		String usuario = usuarioTextView.getText().toString();
+		
+		EditText senhaTextView = (EditText) findViewById(R.id.senha);
+		String senha = senhaTextView.getText().toString();
+		
+		Intent intent = getIntent();
+		Bundle params = intent.getExtras();
+		
+		if(this.testarCadastro(params.getInt("opção"), usuario, senha))
+		{
+			if(params.getInt("opção")==1)
+				intent = new Intent(this,GestorActivity.class);
+			else
+			if(params.getInt("opção")==2)
+				intent = new Intent(this,DocenteActivity.class);
+			else
+				intent = new Intent(this,DiscenteActivity.class);
+			startActivity(intent);
+			finish();
+		}
+		else
+		{
+			Toast toast = Toast.makeText(this, "Cadastro não existente!", Toast.LENGTH_SHORT);
+			toast.show();			
+		}
+	}
+	
+	public boolean testarCadastro(int i, String usuario, String senha)
+	{
+		switch(i)
+		{
+			case 1:
+				if ((usuario.toLowerCase().equals("marcia")) && (senha.equals("12345")))
+					return true;
+				break;
+			case 2:
+				if ((usuario.toLowerCase().equals("rhavy")) && (senha.equals("12345")))
+					return true;
+				break;
+			case 3:
+				if ((usuario.toLowerCase().equals("emanuel")) && (senha.equals("12345")))
+					return true;
+				break;
+		}
+		return false;
+	}
+}
---
+++ quality-manager_ANDROID/src/br/edu/ifpb/MenuAplicacaoActivity.java
@@ -0,0 +1,60 @@
+package br.edu.ifpb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class MenuAplicacaoActivity extends Activity implements OnClickListener
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+		
+		ImageButton gestor = (ImageButton) findViewById(R.id.imageButton1);
+		ImageButton docente = (ImageButton) findViewById(R.id.imageButton2);
+		ImageButton discente = (ImageButton) findViewById(R.id.imageButton3);
+		
+		gestor.setOnClickListener(this);
+		docente.setOnClickListener(this);
+		discente.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) 
+	{
+		Intent opção;
+		Bundle params = new Bundle();
+		
+		switch(v.getId())
+		{
+			case R.id.imageButton1:
+				opção = new Intent(this, LoginActivity.class);
+				params.putInt("opção", 1);
+				opção.putExtras(params);
+				startActivity(opção);
+				finish();
+				break;
+			case R.id.imageButton2:
+				opção = new Intent(this, LoginActivity.class);
+				params.putInt("opção", 2);
+				opção.putExtras(params);
+				startActivity(opção);
+				finish();
+				break;
+			case R.id.imageButton3:
+				opção = new Intent(this, LoginActivity.class);
+				params.putInt("opção", 3);
+				opção.putExtras(params);
+				startActivity(opção);
+				finish();
+				break;
+		}
+	}
+
+}
