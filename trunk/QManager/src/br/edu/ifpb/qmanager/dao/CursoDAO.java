f9c3bc64eba64cf8422be4352352b5a1d0b4dca2
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-08-25 20:35:27 +0000

    Reestruturação dos pacotes da aplicação: br.edu.ifpb.qmanager.

---
+++ trunk/QManager/src/br/edu/ifpb/qmanager/dao/CursoDAO.java
@@ -0,0 +1,177 @@
+package br.edu.ifpb.qmanager.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import principal.Banco;
+import br.edu.ifpb.qmanager.entidade.Curso;
+import br.edu.ifpb.qmanager.excecao.ClasseInvalidaException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+
+/*
+ TABLE `curso`
+ `id_curso` INT NOT NULL,
+ `nm_curso` VARCHAR(45) NOT NULL,
+ */
+
+public class CursoDAO implements GenericDAO<Integer, Curso> {
+
+	// a conexão com o banco de dados
+	public Connection connection;
+
+	public CursoDAO(Banco banco) {
+		this.connection = (Connection) banco.getConnection();
+	}
+
+	@Override
+	public int insert(Curso curso) throws ClasseInvalidaException {
+
+		int idCurso = 0;
+
+		try {
+
+			// Define um insert com os atributos e cada valor é representado
+			// por ?
+			String sql = String
+					.format("%s %s ('%s')",
+							"INSERT INTO `tb_curso` (`nm_curso`)",
+							"VALUES", curso.getNomeCurso());
+			
+			// prepared statement para inserção
+			PreparedStatement stmt = (PreparedStatement) connection
+					.prepareStatement(sql);
+
+			stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+
+			idCurso = BancoUtil.getGenerateKey(stmt);
+
+			stmt.close();
+
+		} catch (SQLException sqle) {
+			Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE,
+					null, sqle);
+		}
+
+		return idCurso;
+	}
+
+	@Override
+	public Curso getById(Integer id) throws ClasseInvalidaException {
+
+		Curso curso = null;
+
+		try {
+
+			String sql = String.format("%s %d",
+					"SELECT C.id_curso, C.nm_curso, C.dt_registro FROM `tb_curso` C"
+					+ " WHERE `id_curso` =", id);
+
+			// prepared statement para inserção
+			PreparedStatement stmt = (PreparedStatement) connection
+					.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery(sql);
+
+			List<Curso> instituicoes = convertToList(rs);
+
+			curso = instituicoes.get(0);
+
+		} catch (SQLException sqle) {
+			Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE,
+					null, sqle);
+		}
+
+		return curso;
+	}
+
+	@Override
+	public void update(Curso curso) throws ClasseInvalidaException {
+
+		try {
+
+			// Define update setando cada atributo e cada valor é
+			// representado por ?
+			String sql = "UPDATE `tb_curso` SET `nm_curso`=? "
+					+ "WHERE `id_curso`=?";
+
+			// prepared statement para inserção
+			PreparedStatement stmt = (PreparedStatement) connection
+					.prepareStatement(sql);
+
+			// seta os valores
+			stmt.setString(1, curso.getNomeCurso());
+			stmt.setInt(2, curso.getIdCurso());
+
+			// envia para o Banco e fecha o objeto
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException sqle) {
+			Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE,
+					null, sqle);
+		}
+	}
+
+	@Override
+	public void delete(Curso curso) throws ClasseInvalidaException {
+
+		try {
+
+			// Deleta uma tupla setando o atributo de identificação com
+			// valor representado por ?
+			String sql = "DELETE FROM `tb_curso` WHERE `id_curso`=?";
+
+			// prepared statement para inserção
+			PreparedStatement stmt = (PreparedStatement) connection
+					.prepareStatement(sql);
+
+			// seta os valores
+			stmt.setInt(1, curso.getIdCurso());
+
+			// envia para o Banco e fecha o objeto
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException sqle) {
+			Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE,
+					null, sqle);
+		}
+	}
+
+	@Override
+	public List<Curso> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Curso> convertToList(ResultSet rs) {
+
+		List<Curso> cursos = new ArrayList<Curso>();
+
+		Curso curso = new Curso();
+
+		try {
+			while (rs.next()) {
+				curso.setIdCurso(rs.getInt("id_curso"));
+				curso.setNomeCurso(rs.getString("nm_curso"));
+				curso.setRegistro(rs.getDate("dt_registro"));
+			}
+			
+			cursos.add(curso);
+			
+		} catch (SQLException e) {
+			Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE,
+					null, e);
+		}
+		
+		return cursos;
+	}
+}
---
+++ trunk/QManager/src/br/edu/ifpb/qmanager/dao/TurmaDAO.java
@@ -0,0 +1,204 @@
+package br.edu.ifpb.qmanager.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import principal.Banco;
+import br.edu.ifpb.qmanager.entidade.Curso;
+import br.edu.ifpb.qmanager.entidade.Turma;
+import br.edu.ifpb.qmanager.excecao.ClasseInvalidaException;
+import br.edu.ifpb.qmanager.util.Constantes;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+
+/*
+ TABLE `turma`
+ `id_turma` INT NOT NULL AUTO_INCREMENT,
+ `nr_ano` INT NOT NULL,
+ `nm_turno` CHAR NOT NULL,
+ `curso_id` INT NOT NULL
+ */
+//TODO: implements DAO
+public class TurmaDAO implements GenericDAO<Integer, Turma> {
+
+	// a conexão com o banco de dados
+	public Banco banco;
+	public Connection connection;
+
+	public TurmaDAO(Banco banco) {
+		this.banco = banco;
+		this.connection = (Connection) banco.getConnection();
+	}
+
+	public int insert(Turma turma) throws ClasseInvalidaException {
+
+		int idTurma = 0;
+
+		try {
+
+			//TODO: Verificar se o código do curso já está cadastrado. Caso não esteja inserir Curso antes da Turma.
+			int idCurso = turma.getCurso().getIdCurso();
+			if (idCurso == Constantes.ID_VAZIO) {
+				CursoDAO cursoDAO = new CursoDAO(this.banco);
+				idCurso = cursoDAO.insert(turma.getCurso()); 
+			}
+			
+			// Define um insert com os atributos e cada valor é representado
+			// por ?
+			String sql = String.format("%s %s ('%s', '%s', '%s')",
+					"INSERT INTO `turma` (`nr_ano`, `nm_turno`, `curso_id`)",
+					"VALUES", turma.getAno(), turma.getTurno(), turma
+							.getCurso().getIdCurso());
+
+			// prepared statement para inserção
+			PreparedStatement stmt = (PreparedStatement) connection
+					.prepareStatement(sql);
+
+			stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+
+			idTurma = BancoUtil.getGenerateKey(stmt);
+
+			stmt.close();
+
+		} catch (SQLException sqle) {
+			throw new RuntimeException(sqle);
+		}
+
+		return idTurma;
+	}
+
+	public Turma getById(Integer id) throws ClasseInvalidaException {
+
+		Turma turma = null;
+
+		try {
+			
+			String sql = String.format("%s %d",
+					"SELECT T.nr_ano, T.nm_turno, T.curso_id FROM `turma` T"
+					+ " WHERE `id_turma` =", id);
+
+			// prepared statement para inserção
+			PreparedStatement stmt = (PreparedStatement) connection
+					.prepareStatement(sql);
+			
+			ResultSet rs = stmt.getResultSet();
+			
+			List<Turma> turmas = convertToList(rs);
+			
+			turma = turmas.get(0);
+
+		} catch (SQLException sqle) {
+			throw new RuntimeException(sqle);
+		}
+
+		return turma;
+
+	}
+
+	public void update(Turma entidade) throws ClasseInvalidaException {
+
+		if (entidade instanceof Turma) {
+
+			Turma turma = (Turma) entidade;
+
+			try {
+
+				// Define update setando cada atributo e cada valor é
+				// representado por ?
+				String sql = "UPDATE `turma` SET `nr_ano`=?, `nm_turno`=?, `curso_id`=? "
+						+ "WHERE `id_turma`=?";
+
+				// prepared statement para inserção
+				PreparedStatement stmt = (PreparedStatement) connection
+						.prepareStatement(sql);
+
+				// seta os valores
+				stmt.setInt(1, turma.getAno());
+				stmt.setString(2, turma.getTurno());
+				stmt.setInt(3, turma.getCurso().getIdCurso());
+				stmt.setInt(4, turma.getIdTurma());
+
+				// envia para o Banco e fecha o objeto
+				stmt.execute();
+				stmt.close();
+
+			} catch (SQLException sqle) {
+				throw new RuntimeException(sqle);
+			}
+
+		} else {
+			throw new ClasseInvalidaException();
+		}
+
+	}
+
+	public void delete(Turma entidade) throws ClasseInvalidaException {
+
+		if (entidade instanceof Turma) {
+
+			Turma turma = (Turma) entidade;
+
+			try {
+
+				// Deleta uma tupla setando o atributo de identificação com
+				// valor representado por ?
+				String sql = "DELETE FROM `turma` WHERE `id_turma`=?";
+
+				// prepared statement para inserção
+				PreparedStatement stmt = (PreparedStatement) connection
+						.prepareStatement(sql);
+
+				// seta os valores
+				stmt.setInt(1, turma.getIdTurma());
+
+				// envia para o Banco e fecha o objeto
+				stmt.execute();
+				stmt.close();
+			} catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
+
+		}
+	}
+
+	@Override
+	public List<Turma> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Turma> convertToList(ResultSet rs) {
+		
+		List<Turma> turmas = new ArrayList<Turma>();
+
+		Turma turma = new Turma();
+		
+		try {
+			
+			while (rs.next()) {
+				turma.setAno(rs.getInt("nr_ano"));
+				turma.setTurno(rs.getString("nm_turno"));
+
+				Curso curso = new Curso();
+				curso.setIdCurso(rs.getInt("curso_id"));
+				turma.setCurso(curso);
+			}
+			
+			turmas.add(turma);
+			
+		} catch (SQLException e) {
+			Logger.getLogger(TurmaDAO.class.getName()).log(Level.SEVERE,
+					null, e);
+		}		
+
+		return turmas;
+	}
+
+}
