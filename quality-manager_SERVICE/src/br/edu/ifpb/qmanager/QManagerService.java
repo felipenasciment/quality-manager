504bed7f8de035dfa649c965a7276b04678f8bc0
emanuelguimaraes23@gmail.com <emanuelguimaraes23@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-08-26 16:52:31 +0000

    Atualização quality-manager_SERVICE

--- quality-manager_SERVICE/src/br/edu/ifpb/qmanager/QManagerService.java
+++ quality-manager_SERVICE/src/br/edu/ifpb/qmanager/QManagerService.java
@@ -1,6 +1,15 @@
 package br.edu.ifpb.qmanager;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import br.edu.ifpb.qmanager.entidade.Instituicao;
+import br.edu.ifpb.teste.Server;
 
 @Path("service")
 public class QManagerService {
@@ -37,15 +46,48 @@ public class QManagerService {
 		Item item1 = new Item();
 		item1.setDescription("produto1");
 		item1.setPrice(2500);
-		
+
 		itens[0] = item1;
-		
+
 		Item item2 = new Item();
 		item2.setDescription("produto2");
 		item2.setPrice(3500);
-		
+
 		itens[1] = item2;
 
 		return itens;
 	}
+
+	@POST
+	@Path("/send")
+	@Consumes("application/json")
+	public Response consumeJSON(Instituicao instituicao) {
+
+		// Banco banco = new Banco();
+
+		// banco.iniciarConexao("root", "ifpbinfo");
+
+		// InstituicaoDAO instituicaoDAO = new InstituicaoDAO(banco);
+
+		// instituicaoDAO.creat(instituicao);
+
+		// banco.encerrarConexao();
+
+		String output = instituicao.toString();
+
+		return Response.status(200).entity(output).build();
+	}
+
+	@GET
+	@Path("servidoronline")
+	@Produces("application/json")
+	public Server digaOlaServer() {
+
+		Server server = new Server();
+		server.setOnline(true);
+
+		return server;
+
+	}
+
 }
