ad35fa9c4e29195dda6806bcf663ca97d7c98b6c
emanuelguimaraes23@gmail.com <emanuelguimaraes23@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-08-26 16:56:22 +0000

    Atualização quality-manager_WEB

---
+++ trunk/quality-manager_WEB/src/main/java/br/edu/ifpb/qmanager/QManagerClient.java
@@ -0,0 +1,62 @@
+package br.edu.ifpb.qmanager;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+import br.edu.ifpb.qmanager.entidade.Instituicao;
+
+
+public class QManagerClient {
+
+	// Deixar este método comum para todas as passadas do Servlet pra cá, e
+	// depois, mandar pra o WebService
+	public static String requestClient(Instituicao instituicao) {
+
+		// Student st = new Student("Catain", "Hook", 10, 12);
+
+		/*
+		 * Alternatively you can use this simple String to send instead of using
+		 * a Student instance
+		 * 
+		 * String jsonString =
+		 * "{\"id\":12,\"firstName\":\"Catain\",\"lastName\":\"Hook\",\"age\":10}"
+		 * ;
+		 */
+
+		String aux = null;
+
+		try {
+			ResteasyClient client = new ResteasyClientBuilder().build();
+
+			ResteasyWebTarget target = client
+					.target("http://localhost:8080/QManager_SERVICE/service/send");
+
+			Response response = target.request().post(
+					Entity.entity(instituicao, "application/json"));
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+			}
+
+			aux = response.readEntity(String.class);
+			// System.out.println("Server response : \n");
+			// System.out.println(response.readEntity(String.class))
+
+			response.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		return aux;
+
+	}
+
+}
---
+++ trunk/quality-manager_WEB/src/main/java/servlets/ControleInstituicao.java
@@ -0,0 +1,52 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.edu.ifpb.qmanager.QManagerClient;
+import br.edu.ifpb.qmanager.entidade.Instituicao;
+import br.edu.ifpb.qmanager.util.StringUtil;
+
+@WebServlet("/ControleDiscente")
+public class ControleInstituicao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Instituicao instituicao = new Instituicao();
+
+		instituicao.setCnpj(request.getParameter("nm_cnpj"));
+		instituicao.setNomeInstituicao(request.getParameter("nm_instituicao"));
+		instituicao.setSigla(request.getParameter("nm_sigla"));
+		instituicao.setOrcamento(StringUtil.tirarMascara(request.getParameter("vl_orcamento")));
+		
+		String aux = QManagerClient.requestClient(instituicao);
+		
+		response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();        
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Sample Application Servlet Page</title>");
+        writer.println("</head>");
+        writer.println("<body bgcolor=white>");
+
+        writer.println("<h1>Retorno do JSON</h1>");
+        
+        writer.println(aux);
+        writer.println("Espero que funcione!!!");
+
+        writer.println("</body>");
+        writer.println("</html>");
+		
+		//response.sendRedirect("paginaGestor.html");
+
+	}
+
+}
