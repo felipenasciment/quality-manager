ae1f604b5ea9eed6de50f550470808395d2a6f34
emanuelguimaraes23@gmail.com <emanuelguimaraes23@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-08-27 20:28:19 +0000

    Commit atualizado do quality-manager_ANDROID (Splash Screen atualizada)

--- trunk/quality-manager_ANDROID/src/br/edu/ifpb/SplashScreenActivity.java
+++ trunk/quality-manager_ANDROID/src/br/edu/ifpb/SplashScreenActivity.java
@@ -1,31 +1,27 @@
 package br.edu.ifpb;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreenActivity extends Activity implements Runnable
 {
 
-	private final int DURAÇAO_SPLASH_SCREEN = 2000;
+	private static int SPLASH_TIME_OUT = 2000;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-		
-		Handler hand = new Handler();
-		hand.postDelayed(this, DURAÇAO_SPLASH_SCREEN);
-	}
-	
-	@Override
-	public void run() 
-	{
-		Intent mudança_tela = new Intent(this, MenuAplicacaoActivity.class);
-		startActivity(mudança_tela);
-		finish();
+		Handler handler = new Handler();
+        handler.postDelayed(this, SPLASH_TIME_OUT);
 	}
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		VerificaConexaoAsyncTask verifica_conexao = new VerificaConexaoAsyncTask(this);
+		verifica_conexao.execute();
+	}
 }
