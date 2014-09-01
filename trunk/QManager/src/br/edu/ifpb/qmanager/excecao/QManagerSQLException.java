a1093f2a0a65d341c0827b8ec2d149c91774570f
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-09-01 18:49:12 +0000

    QManagerSQLException melhorada - muito embora seria bastante interessante conseguir uma chave de acesso pro google tradutor - e Util.Metodos recebeu um novo método.

--- trunk/QManager/src/br/edu/ifpb/qmanager/excecao/QManagerSQLException.java
+++ trunk/QManager/src/br/edu/ifpb/qmanager/excecao/QManagerSQLException.java
@@ -18,7 +18,9 @@ public class QManagerSQLException extends SQLException {
 		erros.put(1054, "Coluna desconhecida: ");
 		erros.put(1146, "Tabela não existe: ");
 		erros.put(1406, "Dado muito longo para a coluna: ");
-		erros.put(1451, "Não é possível excluir ou atualizar uma linha pai: uma restrição de chave estrangeira falhou: ");
+		erros.put(
+				1451,
+				"Não é possível excluir ou atualizar uma linha pai: uma restrição de chave estrangeira falhou: ");
 	}
 
 	public QManagerSQLException(int errorCode, String localizedMessage) {
@@ -28,35 +30,39 @@ public class QManagerSQLException extends SQLException {
 	}
 
 	private static String parseMessage(int errorCode, String localizedMessage) {
-		
+
 		ArrayList<Integer> vet = new ArrayList<Integer>();
 		ArrayList<String> pal = new ArrayList<String>();
-		
-		// TODO: não tratamos o caso em que há apenas a mensagem que definimos a ser exibida
-		
+
+		// TODO: não tratamos o caso em que há apenas a mensagem que definimos a
+		// ser exibida
+
 		// descobre limites das palavras que nos interessam
 		for (int i = 0; i < localizedMessage.length(); i++) {
-			if(localizedMessage.charAt(i) == '\'') {
+			if (localizedMessage.charAt(i) == '\'') {
 				vet.add(i);
 			}
 		}
-		
+
 		String temp = "";
-		
-		// recuperam as palavras que nos interessam
-		for (int i = 0, j; i < vet.size(); i+=2) {
-			j = vet.get(i) + 1;
-			pal.add(localizedMessage.substring(vet.get(i), vet.get(j)));
+		if (vet.size() > 1) {
+
+			// recuperam as palavras que nos interessam
+			for (int i = 0, j; i < vet.size(); i += 2) {
+				j = vet.get(i) + 1;
+				pal.add(localizedMessage.substring(vet.get(i), vet.get(j)));
+			}
+
+			for (String s : pal) {
+				temp += s;
+			}
+
+			temp = temp.substring(0, temp.length() - 2);
+
 		}
-		
-		for (String s : pal) {
-			temp += s;
-		}
-		
-		temp = temp.substring(0, temp.length()-2);
-		
+
 		return erros.get(errorCode) + temp;
-		
+
 	}
 
 }
--- trunk/QManager/src/br/edu/ifpb/qmanager/util/Metodos.java
+++ trunk/QManager/src/br/edu/ifpb/qmanager/util/Metodos.java
@@ -2,7 +2,7 @@ package br.edu.ifpb.qmanager.util;
 
 public class Metodos {
 
-	public static double tirarMascara(String orcamento) {
+	public static double tirarMascaraOrcamento(String orcamento) {
 
 		orcamento = orcamento.replace(".", "");
 		orcamento = orcamento.replace(",", ".");
@@ -10,6 +10,17 @@ public class Metodos {
 		Double orc = Double.parseDouble(orcamento);
 
 		return orc;
+		
+	}
+	
+	public static String tirarMascaraCNPJ(String cnpj) {
+		
+		cnpj = cnpj.replace(".", "");
+		cnpj = cnpj.replace("/", "");
+		cnpj = cnpj.replace("-", "");
+		
+		return cnpj;
+		
 	}
 
 }
