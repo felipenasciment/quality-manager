9842b410d8e6fb9957cb18d8629148e4f50751be
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-09-02 22:55:56 +0000

    Melhorias no QManagerClient.

--- trunk/quality-manager_WEB/src/main/java/br/edu/ifpb/qmanager/QManagerClient.java
+++ trunk/quality-manager_WEB/src/main/java/br/edu/ifpb/qmanager/QManagerClient.java
@@ -6,56 +6,42 @@ import javax.ws.rs.core.Response;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.primefaces.json.JSONObject;
 
 import br.edu.ifpb.qmanager.entidade.Instituicao;
 
-
 public class QManagerClient {
 
-	// Deixar este m�todo comum para todas as passadas do Servlet pra c�, e
-	// depois, mandar pra o WebService
-	public static String requestClient(Instituicao instituicao) {
+	/**
+	 * Requisitar inserção de Entidade ao WebService
+	 * 
+	 * @author Eri Jonhson
+	 * @param entidade
+	 * @return JSONObject
+	 * */
+	public static JSONObject requestClient(Instituicao instituicao) {
 
-		// Student st = new Student("Catain", "Hook", 10, 12);
+		JSONObject json = null;
 
-		/*
-		 * Alternatively you can use this simple String to send instead of using
-		 * a Student instance
-		 * 
-		 * String jsonString =
-		 * "{\"id\":12,\"firstName\":\"Catain\",\"lastName\":\"Hook\",\"age\":10}"
-		 * ;
-		 */
+		ResteasyClient client = new ResteasyClientBuilder().build();
 
-		String aux = null;
+		ResteasyWebTarget target = client
+				.target("http://localhost:8080/quality-manager_SERVICE/service/cadastrarInstituicao");
 
-		try {
-			ResteasyClient client = new ResteasyClientBuilder().build();
+		// requisita inserção de entidade ao service e espera resposta
+		Response response = target.request().post(
+				Entity.entity(instituicao, "application/json"));
 
-			ResteasyWebTarget target = client
-					.target("http://localhost:8080/quality-manager_SERVICE/service/send");
-
-			Response response = target.request().post(
-					Entity.entity(instituicao, "application/json"));
-
-			if (response.getStatus() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : "
-						+ response.getStatus());
-			}
-
-			aux = response.readEntity(String.class);
-			// System.out.println("Server response : \n");
-			// System.out.println(response.readEntity(String.class))
-
-			response.close();
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
+		if (response.getStatusInfo() != Response.Status.OK) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus());
 		}
 
-		return aux;
+		json = (JSONObject) response.getEntity();
+
+		response.close();
+
+		return json;
 
 	}
 
