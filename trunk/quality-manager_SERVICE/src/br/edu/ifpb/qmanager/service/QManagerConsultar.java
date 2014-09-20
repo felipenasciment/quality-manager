632cdc9d55622cd4380295b433e14d1eb451f0aa
emanuelguimaraes23@gmail.com <emanuelguimaraes23@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-09-20 15:04:05 +0000

    Atualização quality-manager_SERVICE 	- Criação do primeiro Services (Instituição Financiadora) referentes a Consulta (Busca);

--- trunk/quality-manager_SERVICE/src/br/edu/ifpb/qmanager/service/QManagerConsultar.java
+++ trunk/quality-manager_SERVICE/src/br/edu/ifpb/qmanager/service/QManagerConsultar.java
@@ -1,6 +1,19 @@
 package br.edu.ifpb.qmanager.service;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import br.edu.ifpb.qmanager.dao.DatabaseConnection;
+import br.edu.ifpb.qmanager.dao.InstituicaoFinanciadoraDAO;
+import br.edu.ifpb.qmanager.entidade.InstituicaoFinanciadora;
+import br.edu.ifpb.qmanager.entidade.QManagerErro;
+import br.edu.ifpb.qmanager.excecao.QManagerSQLException;
 
 /**
  * Descrição do método e serviço.
@@ -13,4 +26,39 @@ import javax.ws.rs.Path;
 
 @Path("consultar")
 public class QManagerConsultar {
+
+	@GET
+	@Path("/instituicaofinanciadora")
+	@Produces("application/json")
+	public Response consultarInstituicao() {
+
+		ResponseBuilder builder = Response.status(Response.Status.BAD_REQUEST);
+		builder.expires(new Date());
+
+		DatabaseConnection banco = new DatabaseConnection();
+
+		try {
+
+			banco.iniciarConexao();
+
+			InstituicaoFinanciadoraDAO instituicaoDAO = new InstituicaoFinanciadoraDAO(
+					banco);
+			List<InstituicaoFinanciadora> instituicoesFinanciadoras = instituicaoDAO.getAll();
+
+			builder.status(Response.Status.OK);
+			builder.entity(instituicoesFinanciadoras);
+
+		} catch (QManagerSQLException qme) {
+			QManagerErro erro = new QManagerErro();
+			erro.setCodigo(qme.getErrorCode());
+			erro.setMensagem(qme.getMessage());
+
+			builder.status(Response.Status.INTERNAL_SERVER_ERROR).entity(erro);
+		} finally {
+
+			banco.encerrarConexao();
+		}
+
+		return builder.build();
+	}
 }
