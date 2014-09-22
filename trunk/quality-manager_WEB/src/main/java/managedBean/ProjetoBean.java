5edbaf3e0d9bfec3e83823b2aaff4f294a0d39b8
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-09-22 19:58:47 +0000

    Pequenas alterações nas páginas.

--- trunk/quality-manager_WEB/src/main/java/managedBean/ProjetoBean.java
+++ trunk/quality-manager_WEB/src/main/java/managedBean/ProjetoBean.java
@@ -1,9 +1,13 @@
 package managedBean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 import br.edu.ifpb.qmanager.entidade.Projeto;
 
-public class ProjetoBean extends GenericBean<Projeto> implements
-		beanInterface {
+@ManagedBean
+@RequestScoped
+public class ProjetoBean extends GenericBean<Projeto> implements beanInterface {
 
 	private Projeto projeto = new Projeto();
 
@@ -14,7 +18,7 @@ public class ProjetoBean extends GenericBean<Projeto> implements
 	public void setProjeto(Projeto projeto) {
 		this.projeto = projeto;
 	}
-	
+
 	@Override
 	public void save() {
 
