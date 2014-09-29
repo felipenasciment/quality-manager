abc6e3da4a1227eb21efba94692ce7e0ed0c36c5
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-09-29 14:24:38 +0000

    - Select executando com sucesso!!!

--- quality-manager_WEB/src/main/java/managedBean/ProgramaInstitucionalBean.java
+++ quality-manager_WEB/src/main/java/managedBean/ProgramaInstitucionalBean.java
@@ -1,12 +1,13 @@
 package managedBean;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.model.SelectItem;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
 
 import br.edu.ifpb.qmanager.entidade.InstituicaoFinanciadora;
 import br.edu.ifpb.qmanager.entidade.ProgramaInstitucional;
@@ -14,8 +15,7 @@ import br.edu.ifpb.qmanager.entidade.ProgramaInstitucional;
 @ManagedBean
 @RequestScoped
 public class ProgramaInstitucionalBean extends
-		GenericBean<ProgramaInstitucional, InstituicaoFinanciadora> implements
-		beanInterface {
+		GenericBean<ProgramaInstitucional> implements beanInterface {
 
 	private ProgramaInstitucional programaInstitucional = new ProgramaInstitucional();
 
@@ -38,20 +38,27 @@ public class ProgramaInstitucionalBean extends
 
 	public List<SelectItem> getInstituicoesFinanciadoras() {
 
-		List<InstituicaoFinanciadora> lif = requestSelectConsultar(PathServices.CONSULTAR_INSTITUICAO);
+		Response response = requestSelectConsultar(PathServices.CONSULTAR_INSTITUICAO);
 
-		System.out.println(lif); // aqui me escreve vazia
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus());
+		}
+
+		ArrayList<InstituicaoFinanciadora> alif = response
+				.readEntity(new GenericType<ArrayList<InstituicaoFinanciadora>>() {
+				});
+
+		response.close();
 
 		ArrayList<SelectItem> alsi = new ArrayList<SelectItem>();
 
-		/* TODO: colocar instituições financiadoras no Select
-		if (!lif.isEmpty()) {
-			InstituicaoFinanciadora instituicaoFinanciadora;
+		if (!alif.isEmpty()) {
+			InstituicaoFinanciadora instituicaoFinanciadora = null;
 			SelectItem si = new SelectItem();
-			Iterator<InstituicaoFinanciadora> it = lif.iterator();
 
-			while (it.hasNext()) {
-				instituicaoFinanciadora = (InstituicaoFinanciadora) it.next();
+			for (int i = 0; i < alif.size(); i++) {
+				instituicaoFinanciadora = alif.get(i);
 				si.setValue(instituicaoFinanciadora
 						.getIdInstituicaoFinanciadora());
 				si.setLabel(instituicaoFinanciadora.getSigla());
@@ -59,10 +66,10 @@ public class ProgramaInstitucionalBean extends
 			}
 		} else {
 			System.err.println("Erro!");
-		}*/
+		}
 
 		instituicoesFinanciadoras = alsi;
-		
+
 		return instituicoesFinanciadoras;
 
 	}
