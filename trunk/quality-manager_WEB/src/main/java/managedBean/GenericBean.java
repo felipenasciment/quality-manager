3e78edb50a9cfa07126d6764df98386f6490e055
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-09-29 14:24:38 +0000

    - Select executando com sucesso!!!

--- trunk/quality-manager_WEB/src/main/java/managedBean/GenericBean.java
+++ trunk/quality-manager_WEB/src/main/java/managedBean/GenericBean.java
@@ -4,13 +4,14 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
-public class GenericBean<T, S> implements Serializable {
+public class GenericBean<T> implements Serializable {
 
 	public String requestCadastrar(T entidade, String path) {
 
@@ -37,8 +38,9 @@ public class GenericBean<T, S> implements Serializable {
 
 	}
 
-	// ESSE EU NÃO TESTEI, MAS ACREDITO QUE TAMBÉM DÁ ERRO!
-	public List<T> requestConsultar(String path) {
+	// TODO: Não testado devido a falta de xhtml correspondente
+	// Felipe Nascimento está pesquisando como fazer paginação.
+	public List<T> requestGetAll(String path) {
 		List<T> aux = null;
 
 		ResteasyClient client = new ResteasyClientBuilder().build();
@@ -53,7 +55,8 @@ public class GenericBean<T, S> implements Serializable {
 					+ response.getStatus());
 		}
 
-		aux = (List<T>) response.getEntity();
+		aux = response.readEntity(new GenericType<List<T>>() {
+		});
 
 		response.close();
 
@@ -61,41 +64,15 @@ public class GenericBean<T, S> implements Serializable {
 
 	}
 
-	// ESSE É O QUE DÁ ERRO
-	public List<S> requestSelectConsultar(String path) {
-		List<S> aux = null;
-
-		// TODO: chamar o serviço de consulta e obter o retorno correto
-
-		// teste que se mostrou inútil
-		// Client client = ClientBuilder.newBuilder().build();
-		// WebTarget target =
-		// client.target("http://localhost:8080/quality-manager_SERVICE/" +
-		// path);
-		// Response response = target.request().get();
-
-		// testei também com Invocation
-		// Invocation.Builder invocation = target.request();
-		// Response response = invocation.get();
+	public Response requestSelectConsultar(String path) {
 
 		ResteasyClient client = new ResteasyClientBuilder().build();
 		ResteasyWebTarget target = client
 				.target("http://localhost:8080/quality-manager_SERVICE/" + path);
+
 		Response response = target.request().get();
 
-		System.out.println(response.getEntity()); // aqui sempre me escreve null
-
-		if (response.getStatus() != 200) {
-			throw new RuntimeException("Failed : HTTP error code : "
-					+ response.getStatus());
-		}
-
-		aux = (List<S>) response.getEntity();
-
-		response.close();
-
-		return aux;
+		return response;
 
 	}
-
 }
