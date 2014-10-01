ee7362f1815bf493f0373afb3893089d5a66ab7f
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-10-01 14:47:57 +0000

    - Refactoring do DAO em QManager. Controle de banco de dadaos enviado para QManager_SERVICE.

--- QManager/src/br/edu/ifpb/qmanager/util/StringUtil.java
+++ QManager/src/br/edu/ifpb/qmanager/util/StringUtil.java
@@ -1,10 +1,5 @@
 package br.edu.ifpb.qmanager.util;
 
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import br.edu.ifpb.qmanager.excecao.QManagerSQLException;
 
 public class StringUtil {
 
@@ -28,30 +23,4 @@ public class StringUtil {
 		return cnpj;
 
 	}
-
-	/**
-	 * @author Felipe Nascimento
-	 * @param data "dd/MM/yyyy"
-	 * @return Date
-	 * @throws QManagerSQLException
-	 */
-	public static Date converterStringEmDataSQL(String data)
-			throws QManagerSQLException {
-		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
-		java.util.Date dataUtil = null;
-
-		String[] dma = data.split("/");
-		String dataSaida = dma[2] + "-" + dma[1] + "-" + dma[0];
-
-		try {
-			dataUtil = f.parse(dataSaida);
-		} catch (ParseException e) {
-			throw new QManagerSQLException(666, "'" + data + "'");
-		}
-
-		Date dataJDBC = new Date(dataUtil.getTime());
-
-		return dataJDBC;
-	}
-
 }
