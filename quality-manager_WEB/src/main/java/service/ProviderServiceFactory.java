357af2d180e1e58d1f7111e6611b4269186c1293
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-10-16 03:20:35 +0000

    - Correção no método getById da classe TipoPessoa. - Modificações e outras dúvida no LoginBean. - Atualização na classe ProviderServiceFactory

--- quality-manager_WEB/src/main/java/service/ProviderServiceFactory.java
+++ quality-manager_WEB/src/main/java/service/ProviderServiceFactory.java
@@ -1,38 +1,42 @@
 package service;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 public class ProviderServiceFactory {
 
-	private static final String URL_SERVICE = 
-		"http://localhost:8080/quality-manager_SERVICE/";
-	
+	private static final String URL_SERVICE = "http://localhost:8080/quality-manager_SERVICE/";
+
 	static {
 		/*
-		 * Precisa ser chamado uma �nica vez para registrar providers RESTEasy,
+		 * Precisa ser chamado uma única vez para registrar providers RESTEasy,
 		 * scanear classes, etc
 		 */
 		RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
 	}
 
-	public static <T> T createServiceClient(Class<T> serviceType){
+	public static <T> T createServiceClient(Class<T> serviceType) {
 		return createServiceClient(URL_SERVICE, serviceType);
 	}
-	
+
 	/**
 	 * A factory of clients to our REST services.
 	 * 
 	 * @author Rhavy Maia Guedes
+	 * @author Eri Jonhson
 	 */
 	public static <T> T createServiceClient(String serviceUrl,
 			Class<T> serviceType) {
 
-		HttpClient client = new HttpClient();
-		client.getParams().setContentCharset("UTF-8");
+		ResteasyClient client = new ResteasyClientBuilder().build();
 
-		return ProxyFactory.create(serviceType, serviceUrl);
+		ResteasyWebTarget target = client.target(serviceUrl);
+
+		return target.proxy(serviceType);
+
 	}
+
 }
