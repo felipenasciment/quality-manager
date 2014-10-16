739ec88e2d44be32bedcd607131bdeeb6c96962c
erijonhson.os@gmail.com <erijonhson.os@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-10-16 01:32:30 +0000

    - As constantes do banco de dados, com variante nos comentários do código.

--- quality-manager_SERVICE/src/br/edu/ifpb/qmanager/dao/Constantes.java
+++ quality-manager_SERVICE/src/br/edu/ifpb/qmanager/dao/Constantes.java
@@ -3,8 +3,9 @@ package br.edu.ifpb.qmanager.dao;
 public class Constantes {
 
 	public static int ID_VAZIO = 0;
-	public static String USER_DATABASE = "root";
-	public static String PASS_DATABASE = "ifpbinfo";
+	public static String USER_DATABASE = "root"; // qmanager_user
+	public static String PASS_DATABASE = "ifpbinfo"; // qm4n4g3r_us3r
+	// 192.168.1.245:3306/qmanager
 	public static String URL_DATABASE = "jdbc:mysql://localhost:3306/qmanager";
 
 }
