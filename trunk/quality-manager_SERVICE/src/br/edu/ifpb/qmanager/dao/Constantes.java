a9a412bb2dede32f71236712b07c94e705d974de
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-10-17 16:46:57 +0000

    - O Login do usuário traz a entidade Pessoa; - Mudança na classe Pessoa, passou de abstrata para concreta.

--- trunk/quality-manager_SERVICE/src/br/edu/ifpb/qmanager/dao/Constantes.java
+++ trunk/quality-manager_SERVICE/src/br/edu/ifpb/qmanager/dao/Constantes.java
@@ -3,8 +3,8 @@ package br.edu.ifpb.qmanager.dao;
 public class Constantes {
 
 	public static int ID_VAZIO = 0;
-	public static String USER_DATABASE = "root"; // qmanager_user
-	public static String PASS_DATABASE = "ifpbinfo"; // qm4n4g3r_us3r
+	public static String USER_DATABASE = "qmanager_user"; // qmanager_user
+	public static String PASS_DATABASE = "qm4n4g3r_us3r"; // qm4n4g3r_us3r
 	// 192.168.1.245:3306/qmanager
 	public static String URL_DATABASE = "jdbc:mysql://localhost:3306/qmanager";
 
