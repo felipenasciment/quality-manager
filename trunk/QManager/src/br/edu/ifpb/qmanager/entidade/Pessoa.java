e9f4f2236fad1478ee06c5cc4e517c7ec5f640af
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-10-17 16:45:19 +0000

    - Mudança na classe Pessoa, passou de abstrata para concreta.

--- trunk/QManager/src/br/edu/ifpb/qmanager/entidade/Pessoa.java
+++ trunk/QManager/src/br/edu/ifpb/qmanager/entidade/Pessoa.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "pessoa")
-public abstract class Pessoa {
+public class Pessoa {
 
 	private int pessoaId;
 	private String nomePessoa;
