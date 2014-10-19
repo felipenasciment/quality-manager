23d9ebffb368564056214c9875bda72b10dd6f31
nsousa.felipe@gmail.com <nsousa.felipe@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-10-19 13:45:22 +0000

    Correção no redirecionamento, e atualização dos componentes de visualização.

--- quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
+++ quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
@@ -4,6 +4,7 @@ public class PathRedirect {
 	public static final String indexOrientador = "orientador/indexOrientador.jsf";
 	public static final String indexGestor = "gestor/indexGestor.jsf";
 	public static final String indexDiscente = "discente/indexDiscente.jsf";
-	public static final String indexCoordenador = "discente/indexCoordenador.jsf";
+	public static final String indexCoordenador = "coordenador/indexCoordenador.jsf";
+	public static final String index = "../index.jsf";
 
 }
