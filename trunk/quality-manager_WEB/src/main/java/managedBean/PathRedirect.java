3443b12e95649fbb4e0d9d3726c1a3dce1417e7a
nsousa.felipe@gmail.com <nsousa.felipe@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-11-12 19:36:19 +0000

    Atualização complementar a última.

--- trunk/quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
+++ trunk/quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
@@ -1,10 +1,10 @@
 package managedBean;
 
 public class PathRedirect {
-	public static final String indexOrientador = "orientador/indexOrientador.jsf";
-	public static final String indexGestor = "gestor/indexGestor.jsf";
-	public static final String indexDiscente = "discente/indexDiscente.jsf";
-	public static final String indexCoordenador = "coordenador/indexCoordenador.jsf";
+	public static final String indexOrientador = "webapp/orientador/indexOrientador.jsf";
+	public static final String indexGestor = "webapp/gestor/indexGestor.jsf";
+	public static final String indexDiscente = "webapp/discente/indexDiscente.jsf";
+	public static final String indexCoordenador = "webapp/coordenador/indexCoordenador.jsf";
 	public static final String index = "../index.jsf";
 
 }
