ab090442cce80cb8587b3bab66e4f69b89d0761d
nsousa.felipe@gmail.com <nsousa.felipe@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-11-14 20:41:16 +0000

    -Correção no redirecionamento das páginas.

--- quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
+++ quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
@@ -5,6 +5,6 @@ public class PathRedirect {
 	public static final String indexGestor = "webapp/gestor/indexGestor.jsf";
 	public static final String indexDiscente = "webapp/discente/indexDiscente.jsf";
 	public static final String indexCoordenador = "webapp/coordenador/indexCoordenador.jsf";
-	public static final String index = "../index.jsf";
+	public static final String index = "../..";
 
 }
