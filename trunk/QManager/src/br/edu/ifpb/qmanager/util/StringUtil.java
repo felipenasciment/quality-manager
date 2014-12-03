171a6dc542e670650302e60c16cc3c6680fca3fd
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2014-12-03 17:08:13 +0000

    - Criptografia da senha.

--- trunk/QManager/src/br/edu/ifpb/qmanager/util/StringUtil.java
+++ trunk/QManager/src/br/edu/ifpb/qmanager/util/StringUtil.java
@@ -1,5 +1,8 @@
 package br.edu.ifpb.qmanager.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class StringUtil {
 
@@ -22,5 +25,22 @@ public class StringUtil {
 
 		return cnpj;
 
+	}
+
+	public static String criptografar(String valorPlano)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+		MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
+		byte messageDigest[] = algorithm.digest(valorPlano.getBytes("UTF-8"));
+
+		StringBuilder hexString = new StringBuilder();
+
+		for (byte b : messageDigest) {
+			hexString.append(String.format("%02X", 0xFF & b));
+		}
+
+		String senha = hexString.toString();
+
+		return senha;
 	}
 }
