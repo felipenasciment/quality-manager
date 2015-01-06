762ff1004a56f22f4aab370f06f40b183cbb8013
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2015-01-06 13:48:43 +0000

    - Upload de arquivos para o serviço rest.

--- quality-manager_SERVICE/src/br/edu/ifpb/qmanager/service/QManagerApplication.java
+++ quality-manager_SERVICE/src/br/edu/ifpb/qmanager/service/QManagerApplication.java
@@ -11,10 +11,12 @@ public class QManagerApplication extends Application {
 	private Set<Class<?>> empty = new HashSet<Class<?>>();
 
 	public QManagerApplication() {
+		
 		// ADD YOUR RESTFUL RESOURCES HERE
 		this.singletons.add(new QManagerCadastrar());
 		this.singletons.add(new QManagerConsultar());
 		this.singletons.add(new QManagerEditar());
+		this.singletons.add(new UploadFileService());
 	}
 
 	public Set<Class<?>> getClasses() {
