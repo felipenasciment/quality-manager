5e0598264e1ff0406950ca8fffbb82ac9aed9ed5
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2015-01-08 12:41:35 +0000

    - Ajuste no login; - Exemplo de upload de arquivo.

---
+++ quality-manager_WEB/src/main/java/beanServices/ArquivoProjetoBean.java
@@ -0,0 +1,59 @@
+package beanServices;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.servlet.http.Part;
+import javax.ws.rs.core.Response;
+
+import managedBean.GenericBean;
+
+import org.apache.commons.io.IOUtils;
+
+import service.FileUploadForm;
+import service.ProviderServiceFactory;
+import service.QManagerService;
+import util.FileUtil;
+
+@ManagedBean
+@RequestScoped
+public class ArquivoProjetoBean {
+
+	private Part arquivo;
+
+	public void importa() {
+
+		try {
+
+			QManagerService service = ProviderServiceFactory
+					.createServiceClient(QManagerService.class);
+			FileUploadForm fuf = new FileUploadForm();
+
+			InputStream is = this.arquivo.getInputStream();
+			byte[] bytes = IOUtils.toByteArray(is);
+
+			fuf.setFileName(FileUtil.getFileName(arquivo));
+			fuf.setData(bytes);
+			
+			Response response = service.uploadFile(fuf);
+			
+			//TODO: Tratar retorno.
+			System.out.println(response.getStatus());
+
+		} catch (IOException e) {
+			GenericBean.setMessage("erro.uploadArquivo", 
+					FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public Part getArquivo() {
+		return arquivo;
+	}
+
+	public void setArquivo(Part arquivo) {
+		this.arquivo = arquivo;
+	}	
+}
