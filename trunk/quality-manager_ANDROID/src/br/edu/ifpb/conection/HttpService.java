3b1ab8b43237dcde8545012d0e1f1903f31a1d53
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2015-02-01 15:37:05 +0000

    - Ajuste na requisição via HTTP GET para suportar parametros e path.

--- trunk/quality-manager_ANDROID/src/br/edu/ifpb/conection/HttpService.java
+++ trunk/quality-manager_ANDROID/src/br/edu/ifpb/conection/HttpService.java
@@ -2,7 +2,9 @@ package br.edu.ifpb.conection;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -11,9 +13,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
 
@@ -26,9 +30,71 @@ public class HttpService {
 	private static String url = Constantes.URL_WEB_SERVICE;
 
 	// constructor
-	public HttpService() {
-	}
+	public HttpService() {}
 
+	private String addPathToUrl(String service, String pathValue) {
+		
+		if(!service.endsWith("/")) {
+			service += "/";
+	    }
+		
+		service += pathValue;
+		
+		return service;		
+	}
+	
+	private String addParamsToUrl(String service, Map<String, String> parametros){
+	    
+		List<NameValuePair> params = new LinkedList<NameValuePair>();
+		
+		if(!service.endsWith("?")) {
+			service += "?";
+	    }
+		
+		// Iterar os valores do mapa.
+		for (Map.Entry<String, String> entry : parametros.entrySet()) {			
+			
+			params.add(new BasicNameValuePair(entry.getKey(), 
+					String.valueOf(entry.getValue())));
+		}		
+
+	    String paramString = URLEncodedUtils.format(params, HTTP.UTF_8);
+
+	    service += paramString;
+	    
+	    return service;
+	}
+	
+	/**
+	 * Enviar requisição via HTTP GET com valor de path.
+	 * 
+	 * @param service
+	 * @param pathValue
+	 * @return
+	 */
+	public HttpResponse sendGETRequest(String service, String pathValue) {
+		return sendGETRequest(addPathToUrl(service, pathValue));
+	}
+	
+	/**
+	 * Enviar requisição via HTTP GET com parâmetros.
+	 * 
+	 * @param service
+	 * @param parametros
+	 * @return
+	 */
+	public HttpResponse sendGETRequest(String service, 
+			Map<String, String> parametros) {
+		
+		return sendGETRequest(addParamsToUrl(service, parametros));
+	}
+	
+	/**
+	 * Enviar uma requisição ao servidor via HTTP GET.
+	 * 
+	 * @param service
+	 * @return
+	 */
 	public HttpResponse sendGETRequest(String service) {
 
 		HttpResponse response = null;	
@@ -46,6 +112,7 @@ public class HttpService {
 		} catch (IOException e) {
 			Log.e("AsyncTaskKJson", "Error converting result " + e.toString());
 		}
+		
 		return response;
 	}
 	
