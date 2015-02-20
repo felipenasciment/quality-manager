437ff027c22552e844a36eacbc2e29be9e72e57f
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2015-02-20 23:11:08 +0000

    - Release database connection; - Novo Log4j.

---
+++ quality-manager_SERVICE/src/br/edu/ifpb/qmanager/util/PasswordUtil.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.qmanager.util;
+
+import snaq.db.PasswordDecoder;
+
+public class PasswordUtil implements PasswordDecoder{
+
+	@Override
+	public char[] decode(String encoded) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
