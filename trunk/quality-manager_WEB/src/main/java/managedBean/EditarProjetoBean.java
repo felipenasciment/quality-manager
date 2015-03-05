e1c1f3014ce2f8f038a81990e319f67e2c3b0753
nsousa.felipe@gmail.com <nsousa.felipe@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2015-03-05 15:51:15 +0000

    -Novo bean para edição de projeto

---
+++ trunk/quality-manager_WEB/src/main/java/managedBean/EditarProjetoBean.java
@@ -0,0 +1,184 @@
+package managedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+import javax.ws.rs.core.Response;
+
+import org.apache.http.HttpStatus;
+
+import service.ProviderServiceFactory;
+import service.QManagerService;
+import br.edu.ifpb.qmanager.entidade.Campus;
+import br.edu.ifpb.qmanager.entidade.Edital;
+import br.edu.ifpb.qmanager.entidade.Erro;
+import br.edu.ifpb.qmanager.entidade.Projeto;
+
+@ManagedBean(name = "editarProjetoBean")
+@SessionScoped
+public class EditarProjetoBean {
+
+	private Projeto projeto;
+
+	private QManagerService service = ProviderServiceFactory
+			.createServiceClient(QManagerService.class);
+
+	private int PROJETO_NAO_CADASTRADO = 0;
+
+	private List<SelectItem> editais;
+	private List<SelectItem> campus;
+	
+	public EditarProjetoBean(Projeto projeto) {
+		this.setProjeto(projeto);
+	}
+
+	public EditarProjetoBean() {
+		this.setProjeto(new Projeto());
+	}
+
+	public void save() {
+
+		Response response = null;
+
+		if (getProjeto().getIdProjeto() == PROJETO_NAO_CADASTRADO) {
+
+			PessoaBean pessoaBean = (PessoaBean) GenericBean
+					.getSessionValue("pessoaBean");
+			getProjeto().getOrientador().setPessoaId(pessoaBean.getPessoaId());
+			response = service.cadastrarProjeto(getProjeto());
+
+		} else {
+
+			response = service.editarProjeto(getProjeto());
+		}
+
+		int statusCode = response.getStatus();
+
+		if (statusCode == HttpStatus.SC_OK) {
+
+			GenericBean.setMessage("info.sucessoCadastroProjeto",
+					FacesMessage.SEVERITY_INFO);
+			GenericBean.resetSessionScopedBean("editarProjetoBean");
+
+		} else {
+
+			// Http Code: 304. Não modificado.
+			Erro erroResponse = response.readEntity(Erro.class);
+
+			GenericBean.setMessage("erro.cadastroProjeto",
+					FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public String createEdit(Projeto projeto) {
+
+		if (projeto == null) {
+			// Curso ainda não criado.
+			GenericBean.resetSessionScopedBean("editarProjetoBean");
+			GenericBean.sendRedirect(PathRedirect.cadastrarProjeto);
+		} else {
+
+			Response response = service
+					.consultarProjeto(projeto.getIdProjeto());
+
+			// Código de resposta do serviço.
+			int statusCode = response.getStatus();
+
+			if (statusCode == HttpStatus.SC_OK) {
+				// Http Code: 200. Resposta para cadastro realizado com sucesso.
+				Projeto projetoResponse = response.readEntity(Projeto.class);
+
+				// Curso encontrado.
+				this.setProjeto(projetoResponse);
+
+			} else {
+				// Http Code: 404. Curso inexistente.
+				Erro erroResponse = response.readEntity(Erro.class);
+
+				GenericBean.setMessage("erro.projetoInexistente",
+						FacesMessage.SEVERITY_ERROR);
+			}
+		}
+
+		return PathRedirect.cadastrarProjeto;
+	}
+
+	public Projeto getProjeto() {
+		return projeto;
+	}
+
+	public void setProjeto(Projeto projeto) {
+		this.projeto = projeto;
+	}
+
+	public List<SelectItem> getEditais() {
+		if (editais != null) {
+
+			return editais;
+
+		} else {
+
+			List<Edital> editaisConsulta = service
+					.listarEditais();
+
+			editais = new ArrayList<SelectItem>();
+
+			if (!editaisConsulta.isEmpty()) {
+
+				for (Edital edital : editaisConsulta) {
+
+					SelectItem selectItem = new SelectItem();
+					selectItem.setValue(edital
+							.getIdEdital());
+					selectItem.setLabel(edital.getNumAno());
+
+					editais.add(selectItem);
+				}
+			}
+
+			return editais;
+		}
+	}
+
+	public void setEditais(List<SelectItem> editais) {
+		this.editais = editais;
+	}
+
+	public List<SelectItem> getCampus() {
+		if (campus != null) {
+
+			return campus;
+
+		} else {
+
+			List<Campus> campusConsulta = service
+					.listarLocais();
+
+			campus = new ArrayList<SelectItem>();
+
+			if (!campusConsulta.isEmpty()) {
+
+				for (Campus campi : campusConsulta) {
+
+					SelectItem selectItem = new SelectItem();
+					selectItem.setValue(campi
+							.getIdCampusInstitucional());
+					selectItem.setLabel(campi.getNome());
+
+					campus.add(selectItem);
+				}
+			}
+
+			return campus;
+		}
+	}
+
+	public void setCampus(List<SelectItem> campus) {
+		this.campus = campus;
+	}
+
+}
