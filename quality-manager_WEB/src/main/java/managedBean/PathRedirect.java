8b9968fb2719bf35140ba46990b3eea5d635c8c9
rhavy.maia@gmail.com <rhavy.maia@gmail.com@9833e17e-d55b-cf5b-0345-d3cf33e877fb>
2015-03-13 01:50:54 +0000

    - Ativação do cadastro do usuário servidor.

--- quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
+++ quality-manager_WEB/src/main/java/managedBean/PathRedirect.java
@@ -9,7 +9,7 @@ public class PathRedirect {
 	public static final String indexGestor = "gestor/indexGestor.jsf";
 	public static final String indexDiscente = "discente/indexDiscente.jsf";
 	public static final String indexCoordenador = "coordenador/indexCoordenador.jsf";
-	public static final String index = "/quality-manager_WEB";
+	public static final String index = "/QManager_WEB";
 	public static final String webAppBase = "/webapp/";
 	
 	public static final String exibirInstituicaoFinanciadora = "exibirInstituicaoFiananciadora.jsf";
